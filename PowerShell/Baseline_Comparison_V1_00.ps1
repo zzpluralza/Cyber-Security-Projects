28dc13ea0fc6bcf67113ed1769f8958bb8f43da0
Garrett Burt <133286146+zzpluralza@users.noreply.github.com>
2024-07-11 15:21:16 -0600

    Add files via upload
    Comparison ps1 script. Part of the baseline scripts

---
+++ PowerShell/Baseline_Comparison_V1_00.ps1
@@ -0,0 +1,16 @@
+﻿#Comparison script - Used in malware investigations
+#Second baseline should have been run and both output files should be put into this script.
+#Date: 07/11/2024
+#Author: Garrett Burt
+$today = get-date -format o
+$today = $today.split("T")
+$systemname = $env:COMPUTERNAME
+#Put the file path and name in the two variables below
+$Firstbaseline = #Path and filename for original baseline
+$Secondbaseline = #Path and filename for new baseline
+
+$comparisonfile = $systemname + "_Comparison_" + $today[0] + ".txt"
+write-host "Comparing both baseline files"
+Compare-Object (get-content $Firstbaseline) (get-content $Secondbaseline) | format-list | Out-file $comparisonfile
+write-host "Comparison complete. Check the output file, " + $comparisonfile + "."
+ 
