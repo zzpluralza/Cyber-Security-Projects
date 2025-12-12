5f454a55e5a78aa2c34227eb90a6815eb1c93294
Garrett Burt <133286146+zzpluralza@users.noreply.github.com>
2025-12-11 20:00:49 -0700

    Create AnalyzeInstallPrograms.ps1

---
+++ PowerShell/AnalyzeInstallPrograms.ps1
@@ -0,0 +1,19 @@
+#12-11-2025 - Cybersecurity research
+#Script to anlayze csv file containing list of installed programs for entire set of computers. Identifies installed PUAs and exports the complete list of computers and installed PUAs
+
+$inputfile = "big_sample.csv"
+$outputfile= "outputfile.csv"
+
+$matcharray = @("Web AceLauncher", "Manualfinder", "WebNavigator","OneStart", "EpiStart")
+
+$filtercolumn = @("NAME", "DEVICE_TYPE", "SITE_UNIQUE_ID", "OS", "APPLICATION_NAME", "VERSION", "PUBLISHER")
+
+$Checkthis = "APPLICATION_NAME"
+
+$datafrominput = import-csv -path $inputfile
+
+$filtered = $datafrominput | where-object {
+	$_.$Checkthis in $matcharray
+}
+
+$filtered | Select-object -property $filtercolumn | export-csv -path $outputfile -notypeinformation
